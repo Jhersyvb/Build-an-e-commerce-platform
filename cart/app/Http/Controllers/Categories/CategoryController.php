59788dff9cae07434271e91f5cde1f040fe20155
Jhersy <jhersyvb@gmail.com>
2020-07-06 10:37:13 -0500

    06. Category index endpoint

---
+++ cart/app/Http/Controllers/Categories/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Categories;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return CategoryResource::collection(
+            Category::with('children')->parents()->ordered()->get()
+        );
+    }
+}
--- cart/routes/api.php
+++ cart/routes/api.php
@@ -1,3 +1,3 @@
 <?php
 
-//
+Route::resource('categories', 'Categories\CategoryController');
