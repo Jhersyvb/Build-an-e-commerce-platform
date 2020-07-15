1aac68e29ae6a900607082b04bcd1c62f427c4df
Jhersy <jhersyvb@gmail.com>
2020-07-15 09:27:33 -0500

    10. Product show endpoint

--- cart/app/Http/Controllers/Products/ProductController.php
+++ cart/app/Http/Controllers/Products/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Products;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductIndexResource;
 
 class ProductController extends Controller
@@ -14,5 +15,10 @@ class ProductController extends Controller
         $products = Product::paginate(10);
 
         return ProductIndexResource::collection($products);
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
     }
 }
---
+++ cart/tests/Feature/Products/ProductShowTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Products;
+
+use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductShowTest extends TestCase
+{
+    public function test_it_fails_if_a_product_cant_be_found()
+    {
+        $this->json('GET', 'api/products/nope')
+            ->assertStatus(404);
+    }
+
+    public function test_it_shows_a_product()
+    {
+        $product = factory(Product::class)->create();
+
+        $this->json('GET', "api/products/{$product->slug}")
+            ->assertJsonFragment([
+                'id' => $product->id,
+            ]);
+    }
+}
