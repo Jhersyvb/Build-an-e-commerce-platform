f129aab545716de5a3beb5ba7a889c6551ab3a6e
Jhersy <jhersyvb@gmail.com>
2020-07-17 11:02:09 -0500

    34. JSON response profiling

---
+++ cart/app/Http/Middleware/ProfileJsonResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Arr;
+use Illuminate\Http\JsonResponse;
+
+class ProfileJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response instanceof JsonResponse && app('debugbar')->isEnabled() && $request->has('_debug')) {
+            $response->setData($response->getData(true) + [
+                '_debugbar' => Arr::only(app('debugbar')->getData(), 'queries'),
+            ]);
+        }
+
+        return $response;
+    }
+}
