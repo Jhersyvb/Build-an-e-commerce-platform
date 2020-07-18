7b50159067c8581646b0bec0303386e4a3a02afb
Jhersy <jhersyvb@gmail.com>
2020-07-18 11:59:16 -0500

    43. Incrementing quantity when adding

--- cart/app/Cart/Cart.php
+++ cart/app/Cart/Cart.php
@@ -20,12 +20,21 @@ class Cart
         );
     }
 
-    public function getStorePayload($products)
+    protected function getStorePayload($products)
     {
         return collect($products)->keyBy('id')->map(function ($product) {
             return [
-                'quantity' => $product['quantity'],
+                'quantity' => $product['quantity'] + $this->getCurrentQuantity($product['id']),
             ];
         })->toArray();
+    }
+
+    protected function getCurrentQuantity($productId)
+    {
+        if ($product = $this->user->cart->where('id', $productId)->first()) {
+            return $product->pivot->quantity;
+        }
+
+        return 0;
     }
 }
--- cart/tests/Feature/Cart/CartTest.php
+++ cart/tests/Feature/Cart/CartTest.php
@@ -25,4 +25,25 @@ class CartTest extends TestCase
 
         $this->assertCount(1, $user->fresh()->cart);
     }
+
+    public function test_it_increments_quantity_when_adding_more_products()
+    {
+        $product = factory(ProductVariation::class)->create();
+
+        $cart = new Cart(
+            $user = factory(User::class)->create()
+        );
+
+        $cart->add([
+            ['id' => $product->id, 'quantity' => 1]
+        ]);
+
+        $cart = new Cart($user->fresh());
+
+        $cart->add([
+            ['id' => $product->id, 'quantity' => 1]
+        ]);
+
+        $this->assertEquals($user->fresh()->cart->first()->pivot->quantity, 2);
+    }
 }
