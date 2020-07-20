a675cf216495899aa2619a854395b94de865948c
Jhersy <jhersyvb@gmail.com>
2020-07-20 18:51:11 -0500

    66. Toggling default addresses

--- cart/app/Models/Address.php
+++ cart/app/Models/Address.php
@@ -12,7 +12,26 @@ class Address extends Model
         'city',
         'postal_code',
         'country_id',
+        'default',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($address) {
+            if ($address->default) {
+                $address->user->addresses()->update([
+                    'default' => false,
+                ]);
+            }
+        });
+    }
+
+    public function setDefaultAttribute($value)
+    {
+        $this->attributes['default'] = ($value === 'true' || $value ? true : false);
+    }
 
     public function user()
     {
