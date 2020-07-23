3be8fe430050e9b7938675274b4cd949d60db07b
Jhersy <jhersyvb@gmail.com>
2020-07-22 19:30:55 -0500

    98. Orders endpoint

---
+++ cart/tests/Feature/Orders/OrderIndexTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Orders;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Order;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class OrderIndexTest extends TestCase
+{
+    public function test_it_fails_if_user_isnt_authenticated()
+    {
+        $this->json('GET', 'api/orders')
+            ->assertStatus(401);
+    }
+
+    public function test_it_returns_a_collection_of_orders()
+    {
+        $user = factory(User::class)->create();
+
+        $order = factory(Order::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->jsonAs($user, 'GET', 'api/orders')
+            ->assertJsonFragment([
+                'id' => $order->id,
+            ]);
+    }
+
+    public function test_it_orders_by_the_latest_first()
+    {
+        $user = factory(User::class)->create();
+
+        $order = factory(Order::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $anotherOrder = factory(Order::class)->create([
+            'user_id' => $user->id,
+            'created_at' => now()->subDay(),
+        ]);
+
+        $this->jsonAs($user, 'GET', 'api/orders')
+            ->assertSeeInOrder([
+                $order->created_at->toDateTimeString(),
+                $anotherOrder->created_at->toDateTimeString(),
+            ]);
+    }
+
+    public function test_it_has_pagination()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'GET', 'api/orders')
+            ->assertJsonStructure([
+                'links',
+                'meta',
+            ]);
+    }
+}
