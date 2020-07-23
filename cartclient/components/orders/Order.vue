3bf22c0fe1e1b10a8bac1dd7046f874144e87f97
Jhersy <jhersyvb@gmail.com>
2020-07-22 19:46:18 -0500

    100. Order index setup

---
+++ cartclient/components/orders/Order.vue
@@ -0,0 +1,22 @@
+<template>
+  <tr>
+    <td>
+      #1
+    </td>
+    <td>
+      2018-01-01
+    </td>
+    <td>
+      <div><a href="">Product 1</a> <a href="">Product 2</a></div>
+      <template>
+        and 2 more
+      </template>
+    </td>
+    <td>£30.00</td>
+    <td>
+      <span class="tag is-medium">
+        Pending
+      </span>
+    </td>
+  </tr>
+</template>
--- cartclient/pages/orders/index.vue
+++ cartclient/pages/orders/index.vue
@@ -1,3 +1,34 @@
 <template>
-  <p>Orders</p>
+  <div class="section">
+    <div class="container is-fluid">
+      <div class="columns">
+        <div class="column is-12">
+          <h1 class="title is-4">Your orders</h1>
+
+          <article class="message">
+            <div class="message-body">
+              <table class="table is-hoverable is-fullwidth">
+                <tbody>
+                  <Order />
+                </tbody>
+              </table>
+            </div>
+          </article>
+          <p>
+            You have no orders
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Order from '@/components/orders/Order'
+
+export default {
+  components: {
+    Order
+  }
+}
+</script>
