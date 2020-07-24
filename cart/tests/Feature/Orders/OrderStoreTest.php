d4775b15f476e34d0c17a72cbf591bd910173f5b
Jhersy <jhersyvb@gmail.com>
2020-07-23 20:23:19 -0500

    120. Fixing failing 'cart empty' test

--- cart/tests/Feature/Orders/OrderStoreTest.php
+++ cart/tests/Feature/Orders/OrderStoreTest.php
@@ -231,6 +231,14 @@ class OrderStoreTest extends TestCase
 
     protected function orderDependencies(User $user)
     {
+        $stripeCustomer = \Stripe\Customer::create([
+            'email' => $user->email,
+        ]);
+
+        $user->update([
+            'gateway_customer_id' => $stripeCustomer->id,
+        ]);
+
         $address = factory(Address::class)->create([
             'user_id' => $user->id,
         ]);
